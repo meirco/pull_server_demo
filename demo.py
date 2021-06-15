05c447cf4c851fbdc8f7495a736f4daa695e5fb8
meirco <45999243+meirco@users.noreply.github.com>
2021-06-15 12:20:07 +0300

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,17 @@
+def mult(a, b):
+    if b == 0:
+        return 0
+    return a + mult(a, b-1)
+
+
+def pow(d,e):
+    if e == 0:
+        return 1
+    if e == 1:
+        return d
+    return mult(d, pow(d, e-1))
+
+
+if __name__ == '__main__':
+    c = pow(2,3)
+    print(c)
